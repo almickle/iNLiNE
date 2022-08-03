033f2efc4a841b5db32eb91193c9525efb8ec917
Michael Dear <michaelcdear@gmail.com>
2022-08-03 06:48:13 -0600

    styling home hub

--- src/components/HomeHub.jsx
+++ src/components/HomeHub.jsx
@@ -8,13 +8,14 @@ import "./style/hubs/home.css"
 export default ({onLiveShop}) => {
 
     return (
-
-        <div id="home-container">
             <div id="home">
-                <h1 id="main-header" data-heading="i">iNLiNE</h1>
+                <h1 id="main-header">iNLiNE</h1>
+                <span id="linebreak"></span>
+                <div id="fav-list"></div>
+                <button id="liveshop" onClick={onLiveShop}>LiveShop</button>
             </div>
-            <button id="liveshop" onClick={onLiveShop}>LiveShop</button>
-        </div>
 
     )
 }
+
+// data-heading="i"
