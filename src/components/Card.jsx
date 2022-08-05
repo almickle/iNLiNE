07be6bac41c5c94452ee247ee4ad23e86b0b2510
Michael Dear <michaelcdear@gmail.com>
2022-08-04 20:57:33 -0600

    akd

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// stylesheet imports
+import "./style/cards/clothingcard.css"
+
+
+// function component
+function Card( {} ){
+
+    const {name, image, collection, price, description} = clothing
+
+    return(
+        <div id="clothing-card-div">
+        <img id="clothing-image-div" src={image}></img>
+        <h2 id="name">{name}</h2>
+        <br></br>
+        <h2 id="price">{price}</h2>
+        <br></br>
+        <h2 id="collection">{collection}</h2>
+        <br></br>
+        <p id="description">{description}</p>
+        <br></br>
+        <button id="add-to-cart">Add To Cart </button>
+    </div>
+    )
+}
+
+export default Card
